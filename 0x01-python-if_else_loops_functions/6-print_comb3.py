ad79e0c25da823c74282a0b3cd49e2b9055d5d2f
Persky08 <pesky08@gmail.com>
2024-01-16 21:49:57 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for first in range(10):
         if first == 8 and second == 9:
             print("89")
         elif first < second:
-            print(f"{first}{second}", end=", ")
+            print("{}{}".format(first, second), end=", ")

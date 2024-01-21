090a952a5c02c176253c3fd8682a77a8b6a91b13
Persky08 <pesky08@gmail.com>
2024-01-21 19:01:58 +0000

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i != 'c' or i != 'C':
-            return my_string
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string

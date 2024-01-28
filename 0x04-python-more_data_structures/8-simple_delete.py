a36a9ad083091fd4b2325481abbdfc88983e28a8
Persky08 <pesky08@gmail.com>
2024-01-28 20:29:41 +0000

    a function that deletes a key in a dictionary.

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,6 +3,6 @@
 def simple_delete(a_dictionary, key=""):
     if a_dictionary is None:
         return
-    if word in a_dictionary:
-        del a_dictionary[word]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary

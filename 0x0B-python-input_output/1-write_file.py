fa6c46c742254b81fe0a711a3a3e4e25dc490c26
Persky08 <pesky08@gmail.com>
2024-02-20 21:27:37 +0000

    a function that writes a string to a text file (UTF8) and returns the number of characters written:

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A write file module
+"""
+
+def write_file(filename="", text=""):
+    """
+    A function to write to a text
+    """
+    with write(filename, mode='w', encoding='utf-8') as File:
+       return File.write(text)

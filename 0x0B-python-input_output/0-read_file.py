caf3cc03b248ef742d8cdc712942b398050e88b3
Persky08 <pesky08@gmail.com>
2024-02-20 21:14:39 +0000

    a function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-A program that reads from a file and print to stdout
+A read file module
 """
 
 
 def read_file(filename=""):
+    """
+    A function that reads file and prints to stdout
+    """
     with open(filename, mode='r', encoding='utf-8') as file:
         print(file.read())

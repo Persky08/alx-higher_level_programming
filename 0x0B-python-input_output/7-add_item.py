331030f1afe71a081fc20fe1836cf7e2b536ec1f
Persky08 <pesky08@gmail.com>
2024-02-21 10:04:34 +0000

    a script that adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """
-Script that adds all arguments to a Python list and saves them to a file.
+A module to load, add and save to python objects
 """
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 if __name__ == "__main__":
     try:
-        existing_list = load_from_json_file("add_item.json")
+        # open the existing file
+        old_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        existing_list = []
+        # create an empty file when file doesn't exist
+        old_list = []
 
-    new_arguments = sys.argv[1:]
-    updated_list = existing_list + new_arguments
-    save_to_json_file(updated_list, "add_item.json")
+    # Extract new command line arguments
+    new_file = sys.argv[1:]
+
+    # Add new file to old list to create a new list
+    new_list = old_list + new_file
+    # save file to add_item.json
+    save_to_json_file(new_list, "add_item.json"

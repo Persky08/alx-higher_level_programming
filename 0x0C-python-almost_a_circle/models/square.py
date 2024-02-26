254c0863ca7997679e100ddff93e6c99517c37e9
Persky08 <pesky08@gmail.com>
2024-02-26 01:09:40 +0000

    Rectangle and Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+defines a square class which inherits from rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+                'id': self.id,
+                'size': self.size,
+                'x': self.x,
+                'y': self.y
+                }

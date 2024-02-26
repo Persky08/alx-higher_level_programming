5fe66ad3e33fe23618a7efbf2405d5500185af20
Persky08 <pesky08@gmail.com>
2024-02-26 01:36:21 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,4 +19,32 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        defines sharing data representation
+        list_dictinaries:
+                        dictionary
+        """
+        if list_dictionaries is None or list_dictionaries == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """
+        Json string representation of a list to a file
+
+        Args:
+            list_objs(list): a list of instance
+
+        Returns:
+                None
+        """
+        if list_objs is None:
+            list_objs = []
+
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+        with open(filename, mode="w", encoding="utf-s") as file:
+            file.write(json_string)

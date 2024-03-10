6cff000623becb68b35196033135c735be76d96d
Persky08 <pesky08@gmail.com>
2024-03-10 14:41:59 +0000

    a class Square that defines a square by: (based on 1-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,9 +10,9 @@ class Square:
         Args:
             size: the size of the square
         '''
-        self.__size = size
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be > 0")
+        self.__size = size
